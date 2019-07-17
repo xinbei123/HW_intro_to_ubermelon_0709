4052bd5814ff3586670d8a32f3c3e05b595ce7fe
Aileen Liu <aileenliu184@gmail.com>
2019-07-16 23:49:17 -0700

    comments added

--- process.py
+++ process.py
@@ -1,12 +1,19 @@
 log_file = open("um-server-01.txt")
-
+#opening the file
 
 def sales_reports(log_file):
     for line in log_file:
+    # iterate over the opened file
+
         line = line.rstrip()
+        # strip off the white space after it
+
         day = line[0:3]
-        if day == "Tue":
+        # grab the day value by doing list slicing
+
+        if day == "Mon":
             print(line)
 
+        # print the data when day is Monday
 
 sales_reports(log_file)
